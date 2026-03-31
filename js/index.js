7125d36c9d6a748cf30b1ddda91ebf341af933d3
Sergioba99 <64738043+Sergioba99@users.noreply.github.com>
2026-03-31 21:38:49 +0200

    feat: implement project viewer logic and dynamic component loading in index.js

--- js/index.js
+++ js/index.js
@@ -119,5 +119,5 @@ const PROJECTS = [
       console.error('Error cargando componente', id, err);
     }
   }
-  loadComponent('main-nav', 'html/navigation.html');
-  loadComponent('main-footer', 'html/footer.html');
+loadComponent('main-nav', '/Portfolio.Sergio.B.github.io/html/navigation.html');
+loadComponent('main-footer', '/Portfolio.Sergio.B.github.io/html/footer.html');
